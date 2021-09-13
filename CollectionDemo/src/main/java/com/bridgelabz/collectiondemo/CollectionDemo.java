d3a935e10f65aaecd99d2481671e4b0879568759
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-13 18:55:57 +0530

    ADDED: project is initialised

---
+++ CollectionDemo/src/main/java/com/bridgelabz/collectiondemo/CollectionDemo.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.collectiondemo;
+
+public class CollectionDemo {
+
+	public static void main(String[] args) {
+		listDemo();
+		stackDemo();
+		queueDemo();
+		setDemo();
+		mapDemo();
+
+	}
+
+	private static void mapDemo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void setDemo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void queueDemo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void stackDemo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void listDemo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
